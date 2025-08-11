42bfc143bae75e79eebcf74beabc13a74be15007
Tae Eiam <tatae28274@gmail.com>
2025-08-11 16:24:19 +0700

    Add api response logging

--- app/src/main/java/com/example/omisechallenge/common/NetworkModule.kt
+++ app/src/main/java/com/example/omisechallenge/common/NetworkModule.kt
@@ -5,6 +5,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -13,12 +15,15 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
     private const val BASE_URL = "https://c8d92d0a-6233-4ef7-a229-5a91deb91ea1.mock.pstmn.io"
+    private val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    private val okHttpClient = OkHttpClient.Builder().addInterceptor(logging).build()
 
     @Provides
     @Singleton
     fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
--- app/src/main/java/com/example/omisechallenge/ui/product/ProductViewModel.kt
+++ app/src/main/java/com/example/omisechallenge/ui/product/ProductViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.omisechallenge.ui.product
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.omisechallenge.domain.usecase.StoreUseCase
@@ -14,16 +13,12 @@ class ProductViewModel @Inject constructor(
 ): ViewModel() {
 
     init {
-        Log.d("Test", "init")
         viewModelScope.launch {
-            Log.d("Test", "before get store")
             getStoreInfo()
         }
     }
 
     suspend fun getStoreInfo() {
-        Log.d("Test", "inside get store")
         val storeInfo = storeUseCase.getStoreInfo().toString()
-        Log.d("Test", storeInfo)
     }
 }
