6d7936d9539de5b9847c2ad7496097d97dc407f8
Tae Eiam <tatae28274@gmail.com>
2025-08-13 12:17:50 +0700

    Add UI to Success screen

--- app/src/main/java/com/example/omisechallenge/ui/success/SuccessFragment.kt
+++ app/src/main/java/com/example/omisechallenge/ui/success/SuccessFragment.kt
@@ -1,18 +1,62 @@
 package com.example.omisechallenge.ui.success
 
+import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import com.example.omisechallenge.R
+import com.example.omisechallenge.databinding.FragmentSuccessBinding
 import com.example.omisechallenge.ui.BaseFragment
 
 class SuccessFragment: BaseFragment() {
+    private var _binding: FragmentSuccessBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSuccessBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     override fun initViews(view: View) {
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
     }
 
     override fun initListeners() {
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    // To prevent back to the previous screen
+                    return
+                }
+            }
+        )
+
+        binding.tvContinueOrdering.setOnClickListener {
+            navigateTo(R.id.action_successFragment_to_productFragment)
+        }
+
     }
 
     override fun subscribeToEvents() {
+        // no op
     }
 
     override fun configViews() {
+        // no op
     }
 }
